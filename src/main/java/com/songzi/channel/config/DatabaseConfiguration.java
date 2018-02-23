d2f3d38676b2b858c7afff88e4d2a5c814717cad
jemeryxu@gmail.com <O9i8u7y6>
2018-02-23 15:15:52 +0800

    liquibase 同步

--- src/main/java/com/songzi/channel/config/DatabaseConfiguration.java
+++ src/main/java/com/songzi/channel/config/DatabaseConfiguration.java
@@ -38,7 +38,8 @@ public class DatabaseConfiguration {
             DataSource dataSource, LiquibaseProperties liquibaseProperties) {
 
         // Use liquibase.integration.spring.SpringLiquibase if you don't want Liquibase to start asynchronously
-        SpringLiquibase liquibase = new AsyncSpringLiquibase(taskExecutor, env);
+//        SpringLiquibase liquibase = new AsyncSpringLiquibase(taskExecutor, env);
+        SpringLiquibase liquibase = new SpringLiquibase();
         liquibase.setDataSource(dataSource);
         liquibase.setChangeLog("classpath:config/liquibase/master.xml");
         liquibase.setContexts(liquibaseProperties.getContexts());
