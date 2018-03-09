98ae133e951da03f01934173a6b7d242e9ec4b70
jemeryxu@gmail.com <O9i8u7y6>
2018-03-09 16:32:08 +0800

    ping++ 支付接口live

--- src/main/java/com/songzi/channel/domain/enumeration/PayType.java
+++ src/main/java/com/songzi/channel/domain/enumeration/PayType.java
@@ -4,5 +4,5 @@ package com.songzi.channel.domain.enumeration;
  * The Status enumeration.
  */
 public enum PayType {
-    wx_pub, wx_wap, wx_lite, alipay, alipay_wap, alipay_pc_direct
+    wx_pub, wx_wap, wx_lite, alipay, alipay_wap, alipay_pc_direct, wx
 }
